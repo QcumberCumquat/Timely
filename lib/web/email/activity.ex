87a9f5918c62b40726afa983b1028aa93f459bd6
Thomas Citharel <tcit@tcit.fr>
2021-05-04 18:34:09 +0200

    fixup! WIP
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/web/email/activity.ex
+++ lib/web/email/activity.ex
@@ -7,6 +7,8 @@ defmodule Mobilizon.Web.Email.Activity do
   import Bamboo.Phoenix
   import Mobilizon.Web.Gettext
 
+  alias Mobilizon.Activities.Activity
+  alias Mobilizon.Actors.Actor
   alias Mobilizon.Config
   alias Mobilizon.Web.{Email, Gettext}
 
@@ -25,11 +27,47 @@ defmodule Mobilizon.Web.Email.Activity do
         instance: Config.instance_name()
       )
 
+    activities = chunk_activities(activities)
+
     Email.base_email(to: email, subject: subject)
     |> assign(:locale, locale)
     |> assign(:subject, subject)
-    |> assign(:activities, Enum.take(activities, 10))
-    |> assign(:total_number_activities, length(activities))
+    |> assign(:activities, activities)
+    |> assign(:total_number_activities, map_size(activities))
     |> render(:email_direct_activity)
+  end
+
+  @spec chunk_activities(list()) :: map()
+  defp chunk_activities(activities) do
+    activities
+    |> Enum.reduce(%{}, fn %Activity{group: %Actor{id: group_id}} = activity, acc ->
+      Map.update(acc, group_id, [activity], fn activities -> activities ++ [activity] end)
+    end)
+    |> Enum.map(fn {key, value} ->
+      {key, Enum.sort(value, &(&1.inserted_at <= &2.inserted_at))}
+    end)
+    |> Enum.map(fn {key, value} -> {key, filter_duplicates(value)} end)
+    |> Enum.into(%{})
+  end
+
+  # We filter duplicates when message_params are being the same
+  # so it will probably not catch much things
+  @spec filter_duplicates(list()) :: list()
+  defp filter_duplicates(activities) do
+    Enum.uniq_by(activities, fn %Activity{
+                                  author: %Actor{id: author_id},
+                                  group: %Actor{id: group_id},
+                                  type: type,
+                                  message: message,
+                                  message_params: message_params
+                                } ->
+      %{
+        author_id: author_id,
+        group_id: group_id,
+        type: type,
+        message: message,
+        message_params: message_params
+      }
+    end)
   end
 end
