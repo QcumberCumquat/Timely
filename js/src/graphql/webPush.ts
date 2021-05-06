8e647724aeeb825cd7ed75a5b69fdd948b355690
Thomas Citharel <tcit@tcit.fr>
2021-05-06 19:22:34 +0200

    fixup! Add webpush front-end support
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- js/src/graphql/webPush.ts
+++ js/src/graphql/webPush.ts
@@ -2,8 +2,6 @@ import gql from "graphql-tag";
 
 export const REGISTER_PUSH_MUTATION = gql`
   mutation RegisterPush($endpoint: String!, $keys: PushSubscriptionKeys!) {
-    registerPush(endpoint: $endpoint, keys: $keys) {
-      status
-    }
+    registerPush(endpoint: $endpoint, keys: $keys)
   }
 `;
